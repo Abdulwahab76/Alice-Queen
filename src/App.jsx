86113bd7aaf6605601c74555cfda8944444c4679
Abdulwahab76 <64549162+Abdulwahab76@users.noreply.github.com>
2021-01-08 06:57:56 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,42 +3,7 @@ import "./App.css";
 import Queen from "./Queen1";
 
 function App() {
-    //     const { ref, playState, getAnimation } = useWebAnimations({
-    //         ...flash,
-    //         timing: {
-    //             duration: 2000,
-    //             iterations: Infinity,
-    //         },
-    //     });
-
-    //     const play = () => {
-    //         getAnimation().play();
-    //     };
-
-    //     const pause = () => {
-    //         getAnimation().pause();
-    //     };
-
-    //     const reverse = () => {
-    //         getAnimation().reverse();
-    //     };
-
-    //     const cancel = () => {
-    //         getAnimation().cancel();
-    //     };
-
-    //     const finish = () => {
-    //         getAnimation().finish();
-    //     };
-    //     return (
-    //         <div>
-    //             <div className="target" ref={ref}></div>
-    //             Cureent animation: {playState} <br />
-    //             <button onClick={play}>play </button>
-    //             <button onClick={pause}>pause </button>
-    //         </div>
-    //     );
-    // }
+   
     return (
         <>
             <Queen />
